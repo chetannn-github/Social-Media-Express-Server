f45b8eb00ddc51c20296de36ed2ec0c316842543
Chetan Singh Rajawat <=chetan.rajawat25@gmail.com>
2024-09-12 08:43:11 +0530

    completed : auth routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,12 @@
+import { userModel } from "../models/userModel.js";
+import { generateToken } from "../utils/generateToken.js";
 import { validateEmail } from "../utils/validateEmail.js";
+import bcrypt from "bcrypt"
 
 export const signUpController = async(req, res)=>{ 
     try {
-        let {username, password,email , confirmPassword, gender } = req.body;
-        if(!username || !password || !email || !confirmPassword || !gender ){
+        let {userName, password,email , confirmPassword, gender,fullName } = req.body;
+        if(!userName || !password || !email || !confirmPassword || !gender  ||!fullName){
          return res.json({"error":"Please fill all the fields!!"})  ;
         
         }
@@ -11,22 +14,43 @@ export const signUpController = async(req, res)=>{
         if(!validateEmail(email)){return res.json({"error":"email is not valid"})  ;}
 
         //finding user if it already exist in db 
+        
+         let user = await userModel.findOne({email});
 
+        if(user){
+            return res.status(400).json({error:"Account already exists !!"});
+        }
 
+        user = await userModel.findOne({userName});
+
+        if(user){
+            return res.status(400).json({error:"username already exists !!"});
+        }
         // if user dont exist
         // hash password 
-
-
+        let salt = await bcrypt.genSalt();
+        let hash = await bcrypt.hash(password, salt);
+        
+        let profilePic = `https://avatar.iran.liara.run/public/boy?username=${userName}`;
+        
         // then create a account in db
+        user = new userModel({userName,password:hash,email,fullName,gender,profilePic })
+        await user.save();
+
+        
         
 
         // login krdoo --- token generate krdo jwt se and cookie set krdooo
-
+        let token = generateToken(userName);
+        res.cookie("token", token,{
+            maxAge:1000*60*60*24*30,
+        })
         // user ki details send krdooo 
-
+        
+        
 
         console.log("signup route");
-        res.json({username,password,email,gender})
+        res.json({userName,email,gender,_id:user._id,profilePic})
 
 
     } catch (error) {
@@ -36,33 +60,51 @@ export const signUpController = async(req, res)=>{
   
 }
 
-export const loginController = (req, res)=>{
-    let {username, password } = req.body;
-
-    if(!username || !password  ){
-        return res.json({"error":"Please fill all the fields!!"})  ;
+export const loginController = async(req, res)=>{
+    try {
+        let {userName, password } = req.body;
+        if(!userName || !password  ){
+            return res.json({"error":"Please fill all the fields!!"})  ;
        
+        }
+
+        // user ko db se find kroo 
+        let user = await userModel.findOne({userName});
+    
+
+        // if user dne => redirect krdo signup route pr 
+        if(!user){
+            return  res.json({"error":"username or password is wrong"});
+
+        }
+
+        // if user exist then compare password and hashpassword using bcrypt 
+        let checkPass = bcrypt.compare(password,user.password);
+        if(!checkPass){
+            return  res.json({"error":"username or password is wrong"});
+        }
+
+        // if password matches then token generate kro and cookie set krdooo
+        let token = generateToken(userName);
+        res.cookie("token",token,{
+            maxAge:1000*24*3600*30,
+        })
+        // user ki details send krdooo
+
+        res.json({userName,email:user.email,gender:user.gender,_id:user._id,profilePic:user.profilePic});
+
+
+    } catch (error) {
+        res.json({"error":"something went wrong in login route"});
+        console.log(error.message);
     }
-
-    // user ko db se find kroo 
-
-    // if user dne => redirect krdo signup route pr 
-
-    // if user exist then compare password and hashpassword using bcrypt 
-
-    // if password matches then token generate kro and cookie set krdooo 
-
-    // user ki details send krdooo
-
-
-    console.log("login route");
-    res.json({username:username,password})
+    
 }
 
 export const logoutController = (req, res)=>{
     // cookie ko delete kr do
-    
+    res.cookie("token","");
     console.log("logout route");
-    res.json({"hii":"Hgfd"})
+    res.json({"success":"logout successfull"})
 }
 
---
+++ models/userModel.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose"
+
+// defining the schema of our user means how our each document (row) of collection(table) will look like
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    userName:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    fullName:{
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    gender:{
+        type:String,
+        enum:["male","female"],
+    },
+    profilePic:{
+        type:String,
+        
+    }
+})
+
+
+// on the basis of schema i will create a users named collection 
+
+export const userModel = mongoose.model("User", userSchema);
